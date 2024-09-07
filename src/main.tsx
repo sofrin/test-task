d43719770c8e75ba82307412ce1a9faaaac1afe1
Sofrin <85531415+sofrin@users.noreply.github.com>
2024-09-07 07:30:48 +0700

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Root from './routes/root.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+	},
+]);
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
