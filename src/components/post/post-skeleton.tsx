51a9bf596551f5a771d1a7d4e632120aeeac8e61
Sofrin <85531415+sofrin@users.noreply.github.com>
2024-09-07 17:28:02 +0700

    fix: skeleton html

--- src/components/post/post-skeleton.tsx
+++ src/components/post/post-skeleton.tsx
@@ -1,9 +1,4 @@
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function PostSkeleton() {
@@ -14,11 +9,11 @@ export default function PostSkeleton() {
 					<Skeleton className='w-1/4 h-full'></Skeleton>
 					<Skeleton className='w-1/12 h-full'></Skeleton>
 				</CardTitle>
-				<CardDescription className='w-full h-full flex flex-row'>
+				<div className='w-full h-full flex flex-row'>
 					<Skeleton className='w-1/6 h-full'></Skeleton>
 					{'|'}
 					<Skeleton className='w-1/12 h-full'></Skeleton>
-				</CardDescription>
+				</div>
 			</CardHeader>
 		</Card>
 	);
