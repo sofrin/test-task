c51de8bd5ede68793b26bf4455c4bf2a11013972
Sofrin <85531415+sofrin@users.noreply.github.com>
2024-09-07 17:02:30 +0700

    fix: comment skeleton

--- src/components/comment-skeleton.tsx
+++ src/components/comment-skeleton.tsx
@@ -10,21 +10,21 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function CommentSkeleton() {
 	return (
-		<Card className='border-0 border-l-2 border-primary m-1'>
-			<CardHeader>
+		<Card className='border-0 border-l-2 border-primary m-1 h-48'>
+			<CardHeader className='h-full'>
 				<CardTitle>
-					<Skeleton className='w-1/2' />
+					<Skeleton className='w-1/12 h-full' />
 				</CardTitle>
 				<CardDescription>
-					<Skeleton className='w-1/2' />
+					<Skeleton className='w-1/12 h-full' />
 				</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<Skeleton className='w-1/2' />
+				<Skeleton className='w-1/5 h-full' />
 			</CardContent>
 
 			<CardFooter>
-				<Skeleton className='w-1/2' />
+				<Skeleton className='w-1/12 h-full' />
 			</CardFooter>
 		</Card>
 	);
