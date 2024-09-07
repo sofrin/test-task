bd71fac2b97cbcfb61585d5baf3c896d46d778ed
Sofrin <85531415+sofrin@users.noreply.github.com>
2024-09-07 18:22:03 +0700

    feat: chunk posts fetch

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -19,21 +19,31 @@ export async function loader() {
 	const data = await (
 		await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
 	).json();
-	const postsData = data.slice(0, 99).map(async (id: number) => {
+	const postsData = data.slice(0, 19).map(async (id: number) => {
 		return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
 			(res) => res.json().catch((err) => console.log(err)),
 		);
 	});
-	const posts = Promise.all(postsData).catch((err) => console.log(err));
-	return defer({ posts });
+	const postsData1 = data.slice(20, 99).map(async (id: number) => {
+		return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+			(res) => res.json().catch((err) => console.log(err)),
+		);
+	});
+
+	const posts = await Promise.all(postsData).catch((err) => console.log(err));
+	const posts1 = Promise.all(postsData1).catch((err) => console.log(err));
+	return defer({ posts, posts1 });
 }
 export default function Home() {
 	useLivePageData();
-	const { posts } = useLoaderData() as { posts: Post[] };
+	const { posts, posts1 } = useLoaderData() as {
+		posts: Post[];
+		posts1: Post[];
+	};
 	return (
 		<div className='container mt-4'>
 			<Suspense
-				fallback={new Array(19).fill('_').map((_, i) => (
+				fallback={new Array(20).fill('_').map((_, i) => (
 					<PostSkeleton key={i} />
 				))}
 			>
@@ -41,6 +51,19 @@ export default function Home() {
 					key='posts'
 					errorElement={<p>Error</p>}
 					resolve={posts}
+				>
+					<PostsList />
+				</Await>
+			</Suspense>
+			<Suspense
+				fallback={new Array(20).fill('_').map((_, i) => (
+					<PostSkeleton key={i} />
+				))}
+			>
+				<Await
+					key='posts1'
+					errorElement={<p>Error</p>}
+					resolve={posts1}
 				>
 					<PostsList />
 				</Await>
