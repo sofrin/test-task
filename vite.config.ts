e1868e71381dc148d19c0e03f1cce7e951a61e7d
Sofrin <85531415+sofrin@users.noreply.github.com>
2024-09-07 17:39:21 +0700

    fix: change port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: { port: 3000 },
 	plugins: [react()],
 	resolve: {
 		alias: {
